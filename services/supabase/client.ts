990d54a75add2c61f83de962b57460203e93788d
alcstackdev <alcstackdev@gmail.com>
2025-08-24 00:12:40 +0300

    Update client.ts

--- services/supabase/client.ts
+++ services/supabase/client.ts
@@ -2,8 +2,8 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
 // Supabase Configuration
-const supabaseUrl = "https://uemiawsrgqunnnexrelf.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVlbWlhd3NyZ3F1bm5uZXhyZWxmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU4NjEzMTAsImV4cCI6MjA3MTQzNzMxMH0.JFi7UP356fNt_0UWfj8ZHTKET--qwY63F91gEcvhnVE";
+const supabaseUrl = "";
+const supabaseKey = "";
 
 // Supabase client'ını oluştur
 export const supabase = createClient(supabaseUrl, supabaseKey, {
